97980d8b8e91be0906d560d035f10f37ab64e05c
Assebe <Lambadina1>
2023-03-16 11:21:00 +0000

    changes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -4,9 +4,7 @@ const mongoose = require("mongoose");
 const User = require("../models/User.model");
 const jwt = require("jsonwebtoken");
 
-
 router.get("/profile/:id", async (req, res, next) => {
-  
   const { id } = req.params;
   try {
     const profile = await User.findById(id).populate("favoriteRadios");
@@ -22,27 +20,18 @@ router.put("/profile/edit/:id", async (req, res, next) => {
   console.log(id, name, email);
 
   try {
-    const updatedProfile = await User.findByIdAndUpdate(id, {
-      name,
-      email,
-    }, {new:true});
-    console.log(updatedProfile)
-           const { _id, email, name } = updatedProfile;
-
-           // Create an object that will be set as the token payload
-           const payload = { _id, email, name };
-
-           // Create a JSON Web Token and sign it
-           const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
-             algorithm: "HS256",
-             expiresIn: "6h",
-           });
-
-           console.log(authToken)
-
-           // Send the token as the response
-           res.status(200).json({ authToken: authToken });
-
+    console.log("insode tryyyyt");
+    const updatedProfile = await User.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+      },
+      { new: true }
+    );
+    console.log("______Here");
+    res.json(updatedProfile)
+    
   } catch (error) {
     res.json(error);
   }
